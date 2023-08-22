df1156a1cc3f8bf412ff501f29da86cb8c4f8579
Saulo Henrique Rodrigues <saulohrodrigues@gmail.com>
2023-08-22 17:49:17 -0300

    Cenario 02 - deveIncluirContaComSucesso

--- src/main/java/br/sp/srodrigues/rest/tests/BarrigaTests.java
+++ src/main/java/br/sp/srodrigues/rest/tests/BarrigaTests.java
@@ -2,6 +2,9 @@ package br.sp.srodrigues.rest.tests;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 import br.sp.srodrigues.rest.core.BaseTest;
@@ -17,4 +20,30 @@ public class BarrigaTests extends BaseTest{
 			.statusCode(401)
 		;
 	}
+
+	@Test
+	public void deveIncluirContaComSucesso() {
+		Map<String, String> login = new HashMap();
+		login.put("email", "saulohrodrigues@gmail.com");
+		login.put("senha", "123456");
+		
+		String token =given()
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token");
+		;
+		
+		given()
+			.header("Authorization", "JWT "+ token) // apis mais novas (JWT ou bearer)
+			.body("{\"nome\":\"qualquer conta1\"}")
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(201)			
+		;
+	}
+	
 }
