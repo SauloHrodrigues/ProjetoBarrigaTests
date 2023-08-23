6e6e0887c80f9ea004d725b32a14df65ee1305cb
Saulo Henrique Rodrigues <saulohrodrigues@gmail.com>
2023-08-23 11:38:56 -0300

    Cenario 03 - deveAlterarContaComSucesso()

--- src/main/java/br/sp/srodrigues/rest/tests/BarrigaTests.java
+++ src/main/java/br/sp/srodrigues/rest/tests/BarrigaTests.java
@@ -5,12 +5,31 @@ import static io.restassured.RestAssured.given;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.sp.srodrigues.rest.core.BaseTest;
 
 public class BarrigaTests extends BaseTest{
 
+	private String TOKEN;
+	
+	@Before
+	public void login() {
+		Map<String, String> login = new HashMap();
+		login.put("email", "saulohrodrigues@gmail.com");
+		login.put("senha", "123456");
+		
+		TOKEN =given()
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token");
+		;
+	}
+	
 	@Test
 	public void naoDeveAcessarAPOSemToken() {
 		given()
@@ -23,26 +42,25 @@ public class BarrigaTests extends BaseTest{
 
 	@Test
 	public void deveIncluirContaComSucesso() {
-		Map<String, String> login = new HashMap();
-		login.put("email", "saulohrodrigues@gmail.com");
-		login.put("senha", "123456");
-		
-		String token =given()
-			.body(login)
-		.when()
-			.post("/signin")
-		.then()
-			.statusCode(200)
-			.extract().path("token");
-		;
-		
 		given()
-			.header("Authorization", "JWT "+ token) // apis mais novas (JWT ou bearer)
-			.body("{\"nome\":\"qualquer conta1\"}")
+			.header("Authorization", "JWT "+ TOKEN) // apis mais novas (JWT ou bearer)
+			.body("{\"nome\":\"qualquer conta\"}")
 		.when()
 			.post("/contas")
 		.then()
 			.statusCode(201)			
+		;
+	}
+
+	@Test
+	public void deveAlterarContaComSucesso() {
+		given()
+			.header("Authorization", "JWT "+ TOKEN) // apis mais novas (JWT ou bearer)
+			.body("{\"nome\":\"'qualquer conta - alterada\"}")
+		.when()
+			.put("/contas/1874389")// ID diferente a 
+		.then()
+			.statusCode(200)			
 		;
 	}
 	
